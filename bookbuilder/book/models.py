bcab7f0832d4c3639fba4e06a8357519aaff0ebe
Mark Seaman <Mark.Seaman@imac.net>
2020-09-15 12:50:18 -0600

    Add Data Models to Book Builder, use Admin views.

--- bookbuilder/book/models.py
+++ bookbuilder/book/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+'''
 
-# Create your models here.
+Data Model Classes
+
+* Reader
+    * user*
+* Author
+    * user*
+    * name
+* Book
+    * author*
+    * title
+* Chapter
+    * book*
+    * title
+    * order
+* Paragraph
+    * chapter*
+    * text
+    * order
+* Image
+    * chapter*
+    * src
+    * alt
+    * order
+
+'''
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.pk}. {self.name} {self.user.email}'
+
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.pk}. {self.title} by {self.author.name}'
--- bookbuilder/book/urls.py
+++ bookbuilder/book/urls.py
@@ -1,8 +1,11 @@
+from django.contrib import admin
 from django.urls import path
+
 from .views import PageView
 
 
 urlpatterns = [
+    path(r'admin/', admin.site.urls),
     path('', PageView.as_view()),
     path('<str:template>', PageView.as_view()),
 ]
