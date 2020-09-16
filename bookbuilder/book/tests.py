4c45ad4547a121022107151c0582faa1158e70ef
Mark Seaman <Mark.Seaman@imac.net>
2020-09-16 07:15:48 -0600

    ''

--- bookbuilder/book/tests.py
+++ bookbuilder/book/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
 
-# Create your tests here.
+
+class ViewTests(SimpleTestCase):
+
+    def test_view_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('index.html')
+
+    def test_view_no_page(self):
+        response = self.client.get('/home.html')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('home.html')
+
+    def test_view_missing_template(self):
+        response = self.client.get('/xxx')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('missing.html')
+
+    def test_view_bad_url(self):
+        response = self.client.get('/xxx/home.html')
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed('missing.html')
--- bookbuilder/book/views.py
+++ bookbuilder/book/views.py
@@ -5,7 +5,7 @@ from os.path import exists
 class PageView(TemplateView):
 
     def get_template_names(self):
-        template_name = self.kwargs.get('template', 'index.html')
+        template_name = self.kwargs.get('template','index.html')
         if not exists('templates/' + template_name):
             template_name = 'missing.html'
         return template_name
